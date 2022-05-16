884d731b0a98f55ad0a1372ade22de5db9e5af4e
David <oconned6@tcd.ie>
2022-05-16 13:01:50 +0100

    basic main functionality, doesn't print graph

--- uv_spec/uv_spec.py
+++ uv_spec/uv_spec.py
@@ -1,7 +1,16 @@
-from pre_pros import restructure_data, blank_data, simple_samples, norm_samples
+import pre_pros 
 
 import pandas as pd 
 import sys 
 
+blank_required = input("Does this data require normalisation?\nYes[1], No[0]")
+
+df = pd.read_csv("data/calibration.csv")
+
+
 def main():
-    
+    if blank_required:
+        pre_pros.norm_samples(df).T.plot(xlabel="Wavelength (nm)", ylabel="Absorbance (au)", title="Calibration Spectra", figsize=(15,9))
+
+if __name__ == '__main__':
+    main()
