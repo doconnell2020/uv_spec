2cf6f0a2294242499aecea489e99d007c852e220
David <oconned6@tcd.ie>
2022-05-17 12:45:52 +0100

    simplify to get base functionality

--- uv_spec/uv_spec.py
+++ uv_spec/uv_spec.py
@@ -1,16 +1,17 @@
 import pre_pros 
 
+from matplotlib import pyplot as plt
 import pandas as pd 
 import sys 
 
-blank_required = input("Does this data require normalisation?\nYes[1], No[0]")
+#blank_required = input("Does this data require normalisation?\nYes[1], No[0]\n: ")
 
 df = pd.read_csv("data/calibration.csv")
 
 
 def main():
-    if blank_required:
-        pre_pros.norm_samples(df).T.plot(xlabel="Wavelength (nm)", ylabel="Absorbance (au)", title="Calibration Spectra", figsize=(15,9))
+    pre_pros.norm_samples(df).T.plot(xlabel="Wavelength (nm)", ylabel="Absorbance (au)", title="Calibration Spectra", figsize=(15,9))
+    plt.show()
 
 if __name__ == '__main__':
     main()
