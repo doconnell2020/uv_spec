6b45c35f980231c3a0e312e119f1f5ec55f95c48
David <oconned6@tcd.ie>
2022-05-18 20:28:42 +0100

    renamed pros to proc

--- uv_spec/uv_spec.py
+++ uv_spec/uv_spec.py
@@ -1,4 +1,4 @@
-import plot_spec, pre_pros 
+import plot_spec, pre_proc 
 
 from matplotlib import pyplot as plt
 import pandas as pd 
@@ -8,27 +8,26 @@ import sys
 #blank_required = input("Does this data require normalisation?\nYes[1], No[0]\n: ")
 
 df = pd.read_csv("data/calibration.csv")
+unknown_spl = pd.read_csv("data/sample.csv")
 
-def get_calibration_slope(df):
-    procd_data = pre_pros.norm_samples(df).T
-    max_idx = procd_data[[1]].idxmax().values[0]
-    max_abs = procd_data.loc[max_idx].values
-    conc = [50/i for i in procd_data.columns]
-    slope = linregress(conc, max_abs)[0]
-    return slope
 
-def prep_unknown_sample(df):
-    
+
+
 
 
 
 
 def main():
-    
-    
-    
-    plot_spec.plot_spec(pre_pros.norm_samples(df))
+    plot_spec.plot_spec(pre_proc.norm_samples(df))
     plt.show()
+
+
+    slope, max_idx = pre_proc.get_calibration_data(df)
+    spl_df = pre_proc.restructure_data(unknown_spl)
+    spl_abs_max = spl_df[max_idx][:3].mean()
+    concentration = spl_abs_max/slope
+
+    print("The unknown sample concentration is {0}", concentration)
 
 if __name__ == '__main__':
     main()
