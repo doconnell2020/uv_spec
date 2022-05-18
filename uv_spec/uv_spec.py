2e655703a3494a3ea28b9355aee1fef80dba8697
David <oconned6@tcd.ie>
2022-05-18 13:03:20 +0100

    calibrate func started

--- uv_spec/uv_spec.py
+++ uv_spec/uv_spec.py
@@ -2,11 +2,22 @@ import plot_spec, pre_pros
 
 from matplotlib import pyplot as plt
 import pandas as pd 
+from scipy.stats import linregress
 import sys 
 
 #blank_required = input("Does this data require normalisation?\nYes[1], No[0]\n: ")
 
 df = pd.read_csv("data/calibration.csv")
+
+def calibrate(df):
+    procd_data = pre_pros.norm_samples(df).T
+    max_idx = procd_data[[1]].idxmax().values[0]
+    max_abs = procd_data.loc[max_idx].values
+    conc = [50/i for i in procd_data.columns]
+    slope = linregress(conc, max_abs)[0]
+    
+
+
 
 
 
