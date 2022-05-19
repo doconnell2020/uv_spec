19b88f1c6d3c5d2150e9aa51e355e997dccb6af6
David <oconned6@tcd.ie>
2022-05-19 12:37:25 +0100

    rm plot.py

--- uv_spec/plot_spec.py
+++
@@ -1,6 +0,0 @@
-def plot_spec(df):
-    return df.copy().T.plot(
-        xlabel="Wavelength (nm)", 
-        ylabel="Absorbance (au)", 
-        title="Calibration Spectra", 
-        figsize=(15,9))
