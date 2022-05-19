793dd00a891c1926d9258214b93dcd7a432ea408
David <oconned6@tcd.ie>
2022-05-19 12:50:05 +0100

    mvp with optional normilisation

--- uv_spec/uv_spec.py
+++ uv_spec/uv_spec.py
@@ -1,11 +1,12 @@
-import plot_spec, pre_proc 
+import pre_proc 
 
 from matplotlib import pyplot as plt
 import pandas as pd 
 from scipy.stats import linregress
 import sys 
 
-#blank_required = input("Does this data require normalisation?\nYes[1], No[0]\n: ")
+cali_norm = input("Does the calibration file require normalisation?\nYes[Y], No[N]\n: ")
+sample_norm = input("Does the unknown sample file require normalisation?\nYes[Y], No[N]\n: ")
 
 df = pd.read_csv("data/calibration.csv")
 unknown_spl = pd.read_csv("data/sample.csv")
@@ -18,7 +19,7 @@ def plot_spec(df):
         figsize=(15,9))
 
 def get_calibration_data(df):
-    procd_data = norm_samples(df).T
+    procd_data = df.T
     max_idx = procd_data[[1]].idxmax().values[0]
     max_abs = procd_data.loc[max_idx].values
     conc = [50/i for i in procd_data.columns]
@@ -27,12 +28,21 @@ def get_calibration_data(df):
 
 
 def main():
-    plot_spec(pre_proc.norm_samples(df))
+    if cali_norm.lower() == 'y':
+        calib_df = pre_proc.norm_samples(df)
+    else:
+        calib_df = pre_proc.simple_samples(df)
+    
+    if sample_norm.lower() == 'y':
+        spl_df = pre_proc.norm_samples(unknown_spl)
+    else:
+        spl_df = pre_proc.simple_samples(unknown_spl)
+
+    plot_spec(calib_df)
     plt.show()
 
 
-    slope, max_idx = pre_proc.get_calibration_data(df)
-    spl_df = pre_proc.restructure_data(unknown_spl)
+    slope, max_idx = get_calibration_data(calib_df)
     spl_abs_max = spl_df[max_idx][:3].mean()
     concentration = spl_abs_max/slope
 
